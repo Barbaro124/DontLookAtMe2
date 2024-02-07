bf6571ddaaab86d42d8395544d33fe3356ace245
Christian Ruiz <cruiz@uccs.edu>
2024-02-06 22:04:46 -0700

    Meta/Extra Files

---
+++ Assets/CursorControl.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CursorControl : MonoBehaviour
+{
+    // Add a public array to specify the scene names where cursor visibility should be controlled
+    public string[] scenesToControlCursor;
+
+    void Start()
+    {
+        // Check if the current scene is in the scenesToControlCursor array
+        if (IsSceneInArray(SceneManager.GetActiveScene().name))
+        {
+            // Hide and lock the cursor by default
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            // Show and unlock the cursor
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    bool IsSceneInArray(string sceneName)
+    {
+        // Check if the current scene is in the scenesToControlCursor array
+        foreach (string scene in scenesToControlCursor)
+        {
+            if (sceneName == scene)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
