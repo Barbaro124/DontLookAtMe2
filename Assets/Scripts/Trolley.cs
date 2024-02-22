fa00e7bdd7456ee58ade82915745262f76ac013d
Christian Ruiz <cruiz@uccs.edu>
2024-02-21 18:00:59 -0700

    Force still not being applied

--- Assets/Scripts/Trolley.cs
+++ Assets/Scripts/Trolley.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Trolley : MonoBehaviour
 {
 
-    MeshCollider rb;
+    
     Vector3 trolleyExitPos;
+    float moveSpeed = 1000f;
+
+    private Rigidbody rb;
+    private bool isMoving = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,22 +21,47 @@ public class Trolley : MonoBehaviour
         playerScript.AddTrolleyExitEventListener(trolleyExit);
         Debug.Log("Trolley added trolleyExit method as event");
 
-        rb = gameObject.GetComponent<MeshCollider>();
+        rb = gameObject.GetComponent<Rigidbody>();
         trolleyExitPos = GameObject.FindWithTag("TrolleyExit").transform.position;
     }
 
+    void FixedUpdate()
+    {
+       Vector3 direction = (trolleyExitPos - transform.position).normalized;
+       rb.AddForce(direction * moveSpeed, ForceMode.Acceleration);
+        Debug.Log("applying force...");
+       if (transform.position != trolleyExitPos) 
+        {
+            isMoving = false;
+        }
+        else
+        {
+            isMoving = true;
+        }
 
+    }
     #region Events
     void trolleyExit()
     {
         Debug.Log("trolleyExit Invoked");
-        if (gameObject.transform.position != trolleyExitPos)
+
+        if (!isMoving)
         {
-            Debug.Log("Trolley is moving");
-            //gameObject.transform.position. = (rb.position + trolleyExitPos * Time.deltaTime);
+           // Vector3 direction = (trolleyExitPos - transform.position).normalized;
+          //  rb.velocity = direction * moveSpeed;
         }
             
-        
+            //while (gameObject.transform.position != trolleyExitPos)
+            //{
+            //    float distance = Vector3.Distance(transform.position, trolleyExitPos);
+
+            //    Vector3 exitDirection = trolleyExitPos - transform.position;
+            //    exitDirection.Normalize();
+
+            //    transform.position += exitDirection * Mathf.Min(moveSpeed * Time.deltaTime, distance);
+            //    Debug.Log("Trolley is moving");
+            //}
+
     }
     #endregion
 }
