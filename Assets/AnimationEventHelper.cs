3af622a3b71dcb96816322d4aef347a0f1f7b8ed
Christian R. <cruiz@uccs.edu>
2024-09-12 21:33:09 -0600

    All monster Prefabs Applied

---
+++ Assets/AnimationEventHelper.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventHelper : MonoBehaviour
+{
+    public void OnHidingAnimationEnd()
+    {
+        // Send a message up to the parent object
+        SendMessageUpwards("OnHidingAnimationEnd");
+    }
+}
